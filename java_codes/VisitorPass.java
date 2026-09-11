e76c9bcfae350dc84f40e15d3b49cb7e76c06b39
Blesson Philipose Shaji <blessonshajimemana10@gmail.com>
2026-09-11 10:18:45 +0530

    Add files via upload

---
+++ java_codes/VisitorPass.java
@@ -0,0 +1,39 @@
+public class VisitorPass {
+
+
+    public static void main(String[] args) {
+
+
+        
+        String visitorName = "Asriel";
+
+
+        
+        String visitDate = "17-08-2026";
+
+
+        
+        String hostEmployee = "John Mathew";
+
+
+        
+        int passNumber = 101;
+
+
+        
+        System.out.println("\n\t================================");
+        System.out.println("\t        VISITOR PASS");
+        System.out.println("\t================================");
+
+
+        System.out.printf("\tPass Number   : %d%n", passNumber);
+        System.out.printf("\tVisitor Name  : %s%n", visitorName);
+        System.out.printf("\tVisit Date    : %s%n", visitDate);
+        System.out.printf("\tHost Employee : %s%n", hostEmployee);
+
+
+        System.out.println("\t================================");
+        System.out.println("\t      Welcome to our office!");
+        System.out.println("\t================================");
+    }
+}
